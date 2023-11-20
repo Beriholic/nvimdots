219a8aa3d974d1c4533a87dce604dc787a03e71b
Beriholic <96678707+Beriholic@users.noreply.github.com>
2023-11-21 00:40:46 +0800

    add key map

--- mappings.lua
+++ mappings.lua
@@ -4,12 +4,14 @@ local M = {}
 M.general = {
   n = {
     [";"] = { ":", "enter command mode", opts = { nowait = true } },
-    ["<leader>acru"] = { "<cmd>:CompetiTest run<CR>", "Competitest" },
-    ["<leader>acad"] = { "<cmd>:CompetiTest add_testcase <CR>", "Competitest" },
-    ["<leader>acre"] = { "<cmd>:CompetiTest receive testcases<CR>", "Competitest" },
-    ["<leader>w"]={"<cmd>:HopWord<CR>","Hop"},
-    ["<leader>j"]={"<cmd>:HopLineStart<CR>","Hop"},
-    ["<leader>k"]={"<cmd>:HopLine<CR>","Hop"},
+    ["<leader>acru"] = { "<cmd>:CompetiTest run<CR>", "Competitest run" },
+    ["<leader>acad"] = { "<cmd>:CompetiTest add_testcase <CR>", "Competitest add testcase" },
+    ["<leader>acre"] = { "<cmd>:CompetiTest receive testcases<CR>", "Competitest receive testcases" },
+    ["<leader>ace"] = { "<cmd>:CompetiTest edit_testcase <CR>", "Competitest edit testcases" },
+    ["<leader>acd"] = { "<cmd>:CompetiTest delete_testcase <CR>", "Competitest delete_testcase" },
+    ["<leader>w"]={"<cmd>:HopWord<CR>","Hop word"},
+    ["<leader>j"]={"<cmd>:HopLineStart<CR>","Hop LineStart"},
+    ["<leader>k"]={"<cmd>:HopLine<CR>","Hop Line"},
   },
 
   v = {
