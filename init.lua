425254a03ee77c9d67046a30f701a2dbe8e14aec
Beriholic <96678707+Beriholic@users.noreply.github.com>
2023-11-21 14:08:55 +0800

    add gofmt on save

--- init.lua
+++ init.lua
@@ -36,3 +36,11 @@ end
 g.vscode_snippets_path =vim.fn.stdpath("config") ..  "/lua/custom/snips"
 
 
+local format_sync_grp = vim.api.nvim_create_augroup("GoImport", {})
+vim.api.nvim_create_autocmd("BufWritePre", {
+  pattern = "*.go",
+  callback = function()
+   require('go.format').goimport()
+  end,
+  group = format_sync_grp,
+})
