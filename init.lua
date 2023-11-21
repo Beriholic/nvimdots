89bf0029e604db5319793e0d9ce93f28438d5cda
Beriholic <beriholic@petmail.com>
2023-11-21 15:49:13 +0800

    add keymap

--- init.lua
+++ init.lua
@@ -44,3 +44,9 @@ vim.api.nvim_create_autocmd("BufWritePre", {
   end,
   group = format_sync_grp,
 })
+
+for i = 1, 9, 1 do
+  vim.keymap.set("n", string.format("<A-%s>", i), function()
+    vim.api.nvim_set_current_buf(vim.t.bufs[i])
+  end)
+end
