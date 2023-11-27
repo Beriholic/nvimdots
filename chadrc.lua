1447ab6ccd76031bf80bde869f4d4b1fec030a75
Beriholic <96678707+Beriholic@users.noreply.github.com>
2023-11-27 16:09:43 +0800

    default theme

--- chadrc.lua
+++ chadrc.lua
@@ -5,8 +5,8 @@ local M = {}
 local highlights = require "custom.highlights"
 
 M.ui = {
-  theme = "tokyonight",
-  theme_toggle = { "tokyonight", "tokyonight" },
+  theme = "ayu_dark",
+  theme_toggle = { "ayu_dark", "ayu_dark" },
 
   hl_override = {
     NvDashAscii = {
