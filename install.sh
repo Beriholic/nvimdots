718e23b82d01cc65fa07eef68bb004bd3f45eb73
Beriholic <96678707+Beriholic@users.noreply.github.com>
2024-03-11 15:21:15 +0800

    update

---
+++ install.sh
@@ -0,0 +1,1 @@
+git clone git@github.com:Beriholic/nvimdots.git ~/.config/nvim --depth1 && nvim
--- lua/install.sh
+++
@@ -1,4 +0,0 @@
-git clone https://github.com/NvChad/NvChad ~/.config/nvim --depth 1 &&
-git clone git@github.com:Beriholic/nvimdots.git ~/.config/nvim/lua/custom --depth 1 &&
-nvim
-
