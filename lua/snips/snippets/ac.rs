145f27f54221002efb89555c58b037fedc31814d
Beriholic <beriholic@petalmail.com>
2024-03-12 17:27:48 +0800

    update

--- lua/snips/snippets/ac.rs
+++ lua/snips/snippets/ac.rs
@@ -1,30 +1,72 @@
 #![allow(unused_imports)]
 use std::io::{stdin, stdout, BufWriter};
 type Int=i64;
+use std::io::prelude::*;
 
-#[derive(Default)]
-struct Scanner {
-    buffer: Vec<String>,
+pub struct Scanner<R> {
+    reader: R,
+    buf_str: Vec<u8>,
+    buf_iter: std::str::SplitAsciiWhitespace<'static>,
 }
-impl Scanner {
-    fn next<T: std::str::FromStr>(&mut self) -> T {
+
+impl<R: BufRead> Scanner<R> {
+    pub fn new(reader: R) -> Self {
+        Self { reader, buf_str: Vec::new(), buf_iter: "".split_ascii_whitespace() }
+    }
+    pub fn next<T: std::str::FromStr>(&mut self) -> T {
         loop {
-            if let Some(token) = self.buffer.pop() {
+            if let Some(token) = self.buf_iter.next() {
                 return token.parse().ok().expect("Failed parse");
             }
-            let mut input = String::new();
-            stdin().read_line(&mut input).expect("Failed read");
-            self.buffer = input.split_whitespace().rev().map(String::from).collect();
+            self.buf_str.clear();
+            self.reader.read_until(b'\n', &mut self.buf_str).expect("Failed read");
+            self.buf_iter = unsafe {
+                let slice = std::str::from_utf8_unchecked(&self.buf_str);
+                std::mem::transmute(slice.split_ascii_whitespace())
+            }
         }
     }
 }
-fn main() {
-    let mut scan = Scanner::default();
-    let out = &mut BufWriter::new(stdout());
 
-    let t:Int=scan.next();
-    
-    for _ in 0..t {
-    }
+macro_rules! input {
+    ($scanner:expr, $($r:tt)*) => {
+        input_inne
+
+r!{$scanner, $($r)*}
+    };
 }
 
+macro_rules! input_inner {
+    ($scanner:expr) => {};
+    ($scanner:expr, ) => {};
+    ($scanner:expr, $var:ident : $t:tt $($r:tt)*) => {
+        let $var = read_value!($scanner, $t);
+        input_inner!{$scanner $($r)*}
+    };
+}
+
+macro_rules! read_value {
+    ($scanner:expr, ( $($t:tt),* )) => {
+        ( $(read_value!($scanner, $t)),* )
+    };
+    ($scanner:expr, [ $t:tt ]) => {{
+        let len = read_value!($scanner, usize);
+        read_value!($scanner, [$t; len])
+    }};
+    ($scanner:expr, [ $t:tt ; $len:expr ]) => {
+        (0..$len).map(|_| read_value!($scanner, $t)).collect::<Vec<_>>()
+    };
+    ($scanner:expr, chars) => {
+        read_value!($scanner, String).chars().collect::<Vec<char>>()
+    };
+    ($scanner:expr, $t:ty) => {
+        $scanner.next::<$t>()
+    };
+}
+
+
+fn main() {
+    let mut scanner = Scanner::new(stdin().lock());
+    let out =&mut BufWriter::new(stdout().lock());
+
+}
