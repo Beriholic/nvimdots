d81f2dc6863f953794f955a175cfb14d7f42e6a0
Beriholic <beriholic@petalmail.com>
2025-05-15 18:56:22 +0800

    refactor :recycle:: refactor: remove unused formatters

--- lua/configs/conform.lua
+++ lua/configs/conform.lua
@@ -13,21 +13,13 @@ local options = {
     astro = { "stylelint", "prettierd", "eslint_d" },
     bash = { "shellcheck", "shfmt" },
     c = { "clang-format", lsp_format = "last" },
-    clojure = { "zprint" },
     cpp = { "clang-format", lsp_format = "last" },
-    cs = { "csharpier" }, -- C#
-    csh = { "shellcheck", "beautysh" },
     css = { "stylelint", "prettierd" },
-    elm = { "elm_format" },
     go = { "goimports", "gofmt" },
-    haskell = { "ormolu" },
     html = { "prettierd" },
-    java = { "google-java-format" },
     javascript = { "stylelint", "prettierd", "eslint_d" },
     javascriptreact = { "stylelint", "prettierd", "eslint_d" },
-    ksh = { "shellcheck", "beautysh" },
     lua = { "stylua" }, -- Stylua is already configured as the default formatter for Lua in NvChad, but keeping it here for reference
-    mksh = { "shellcheck", "shfmt" },
     python = function(bufnr)
       if conform.get_formatter_info("ruff_format", bufnr).available then
         return { "ruff_format" }
@@ -35,15 +27,12 @@ local options = {
         return { "isort", "black" }
       end
     end,
-    ruby = { "rubocop" },
     rust = { "rustfmt" },
     sh = { "shellcheck", "shfmt" },
-    svelte = { "stylelint", "prettierd", "eslint_d" },
-    tcsh = { "shellcheck", "beautysh" },
     typescript = { "stylelint", "prettierd", "eslint_d" },
     typescriptreact = { "stylelint", "prettierd", "eslint_d" },
     vue = { "stylelint", "prettierd", "eslint_d" },
-    zsh = { "shellcheck", "beautysh" },
+    nix = { "nixfmt" },
   },
 
   format_on_save = {
