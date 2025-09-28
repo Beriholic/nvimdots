319acb48ea1098ef1b3cd02c69c4a41f3eb9737c
Beriholic <beriholic@petalmail.com>
2025-09-28 22:23:46 +0800

    feat :sparkles:(keymaps): add telescope live grep keymap

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -15,3 +15,6 @@ end, { desc = "lazygit" })
 
 map("n", "<leader>df", "<cmd>:DiffviewOpen<CR>", { desc = "open file diff" })
 map("n", "<leader>dc", "<cmd>:DiffviewClose<CR>", { desc = "close file diff" })
+
+-- telescope
+map("n", "<leader>fw", "<cmd>Telescope live_grep<CR>", { desc = "telescope live grep" })
