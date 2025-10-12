78c63632b7ce5100f42198c6ce56dc37f58e161e
Beriholic <beriholic@petalmail.com>
2025-10-12 13:08:42 +0800

    chore :hammer:(java): remove java language support

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -27,7 +27,6 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.linting.eslint" },
     { import = "lazyvim.plugins.extras.lang.docker" },
     { import = "lazyvim.plugins.extras.lang.go" },
-    { import = "lazyvim.plugins.extras.lang.java" },
     { import = "lazyvim.plugins.extras.lang.json" },
     { import = "lazyvim.plugins.extras.lang.kotlin" },
     { import = "lazyvim.plugins.extras.lang.markdown" },
--- lua/plugins/lang.lua
+++ lua/plugins/lang.lua
@@ -18,53 +18,53 @@ return {
     lazy = false, -- This plugin is already lazy
   },
 
-  {
-    "nvim-java/nvim-java",
-    config = false,
-    dependencies = {
-      {
-        "neovim/nvim-lspconfig",
-        opts = {
-          servers = {
-            -- Your JDTLS configuration goes here
-            jdtls = {
-              settings = {
-                java = {
-                  configuration = {
-                    runtimes = {
-                      {
-                        name = "jdk21",
-                        path = os.getenv("JAVA_HOME") or "",
-                      },
-                    },
-                  },
-                },
-              },
-            },
-          },
-          setup = {
-            jdtls = function()
-              -- Your nvim-java configuration goes here
-              require("java").setup({
-                root_markers = {
-                  "settings.gradle",
-                  "settings.gradle.kts",
-                  "pom.xml",
-                  "build.gradle",
-                  "mvnw",
-                  "gradlew",
-                  "build.gradle",
-                  "build.gradle.kts",
-                },
-              })
-            end,
-          },
-        },
-      },
-    },
-  },
-  {
-    "mfussenegger/nvim-jdtls",
-    enabled = false,
-  },
+  -- {
+  --   "nvim-java/nvim-java",
+  --   config = false,
+  --   dependencies = {
+  --     {
+  --       "neovim/nvim-lspconfig",
+  --       opts = {
+  --         servers = {
+  --           -- Your JDTLS configuration goes here
+  --           jdtls = {
+  --             settings = {
+  --               java = {
+  --                 configuration = {
+  --                   runtimes = {
+  --                     {
+  --                       name = "jdk21",
+  --                       path = os.getenv("JAVA_HOME") or "",
+  --                     },
+  --                   },
+  --                 },
+  --               },
+  --             },
+  --           },
+  --         },
+  --         setup = {
+  --           jdtls = function()
+  --             -- Your nvim-java configuration goes here
+  --             require("java").setup({
+  --               root_markers = {
+  --                 "settings.gradle",
+  --                 "settings.gradle.kts",
+  --                 "pom.xml",
+  --                 "build.gradle",
+  --                 "mvnw",
+  --                 "gradlew",
+  --                 "build.gradle",
+  --                 "build.gradle.kts",
+  --               },
+  --             })
+  --           end,
+  --         },
+  --       },
+  --     },
+  --   },
+  -- },
+  -- {
+  --   "mfussenegger/nvim-jdtls",
+  --   enabled = false,
+  -- },
 }
